711b3a5e718d8c46ee0de74a5be7aaf6b767ae0b
Hugo Louro <hmclouro@gmail.com>
2015-09-14 17:11:05 -0700

    BUG-41848: IoT - Storage Layer - JDBC implementaiton 1. Add more test methods.

--- core/src/test/java/com/hortonworks/iotas/storage/impl/jdbc/JdbcStorageManagerIntegrationTest.java
+++ core/src/test/java/com/hortonworks/iotas/storage/impl/jdbc/JdbcStorageManagerIntegrationTest.java
@@ -20,6 +20,7 @@ package com.hortonworks.iotas.storage.impl.jdbc;
 
 import com.hortonworks.IntegrationTest;
 import com.hortonworks.iotas.catalog.Device;
+import com.hortonworks.iotas.storage.AlreadyExistsException;
 import com.hortonworks.iotas.storage.Storable;
 import com.hortonworks.iotas.storage.StorableKey;
 import com.hortonworks.iotas.storage.StorageManager;
@@ -81,25 +82,27 @@ public class JdbcStorageManagerIntegrationTest extends JdbcIntegrationTest {
 
 
     private void setDevice() {
-        this.device = new Device();
+        device = newDevice("device_id_test", 123L, 456L);
+    }
+
+    private Device newDevice(String deviceId, long version, long dataSourceId) {
+        Device device = new Device();
         Map<String, Object> state = new HashMap<>();
-        state.put(Device.DEVICE_ID, "device_id_test");
-        state.put(Device.VERSION, 123L);
-        state.put(Device.DATA_SOURCE_ID, 456L);
+        state.put(Device.DEVICE_ID, deviceId);
+        state.put(Device.VERSION, version);
+        state.put(Device.DATA_SOURCE_ID, dataSourceId);
         device.fromMap(state);
+        return device;
     }
 
     // ===== Test Methods ====
 
     @Test
     public void testCrud() throws Exception {
-        System.out.println("HUGO_TEST");
-        StorableKey key = null;
+        StorableKey key = device.getStorableKey();;
         Storable retrieved;
         try {
-            key = device.getStorableKey();
-            retrieved = jdbcStorageManager.get(key);
-            assertNull(retrieved);
+            testGet_inexistingKey_null();
             jdbcStorageManager.add(device);
             retrieved = jdbcStorageManager.get(key);
             assertEquals("Instance put and retrieved from database are different", device, retrieved);
@@ -108,6 +111,53 @@ public class JdbcStorageManagerIntegrationTest extends JdbcIntegrationTest {
             retrieved = jdbcStorageManager.get(key);
             assertNull(retrieved);
         }
+    }
+
+    @Test
+    public void testGet_inexistingKey_null() {
+        final StorableKey key = device.getStorableKey();
+        final Storable retrieved = jdbcStorageManager.get(key);
+        assertNull(retrieved);
+    }
+
+    @Test
+    public void testAdd_InexistingStorable_void() {
+        testGet_inexistingKey_null();
+        jdbcStorageManager.add(device);
+        testGet_ExistingStorable_ExistingStorable(device);
+    }
+
+    private void testGet_ExistingStorable_ExistingStorable(Storable existing) {
+        Storable retrieved = jdbcStorageManager.get(existing.getStorableKey());
+        assertEquals("Instance put and retrieved from database are different", existing, retrieved);
+    }
+
+    @Test(expected = AlreadyExistsException.class)
+    public void testAdd_UnequalExistingStorable_AlreadyExistsException() {
+        testAdd_InexistingStorable_void();
+        Map existing = device.toMap();
+        Storable unequalExisting = newDevice(existing.get(Device.DEVICE_ID), existing.get(Device.VERSION), existing.get(Device.VERSION))
+
+
+    }
+
+    @Test
+    public void testAdd_EqualExistingStorable_void() {
+
+    }
+
+    @Test
+    public void testAddOrUpdate_newStorable_newStorable() {
+        testAdd_InexistingStorable_void();
+        Storable update = newDevice("new_device_id_test", 7L, 8L);
+        jdbcStorageManager.addOrUpdate(update);
+        dotestAdd_Storable_Get_StorableKey_Storable(update);
+    }
+
+
+    public void testAdd_newStorable_AlreadyExistsException() {
+
+
     }
 
     // TODO TEST INSERT DUPLICATE KEY
