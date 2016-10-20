aa6c27e622ba4ddbb1e5f5af051707e1d3432dcd
Hugo Louro <hmclouro@gmail.com>
2016-10-20 10:37:05 -0700

    IOT-393: Capture Metadata from clusters   - Rename method to buildZkRootPath

--- streams/catalog/src/main/java/com/hortonworks/iotas/streams/catalog/service/metadata/KafkaMetadataService.java
+++ streams/catalog/src/main/java/com/hortonworks/iotas/streams/catalog/service/metadata/KafkaMetadataService.java
@@ -59,7 +59,7 @@ public class KafkaMetadataService implements AutoCloseable {
     }
 
     public BrokersInfo<String> getBrokerInfoFromZk() throws ZookeeperClientException {
-        final String brokerIdsZkPath = kafkaZkConnection.buildZkFullPath(KAFKA_BROKERS_IDS_ZK_RELATIVE_PATH);
+        final String brokerIdsZkPath = kafkaZkConnection.buildZkRootPath(KAFKA_BROKERS_IDS_ZK_RELATIVE_PATH);
         final List<String> brokerIds = zkCli.getChildren(brokerIdsZkPath);
         List<String> brokerInfo = null;
 
@@ -74,12 +74,12 @@ public class KafkaMetadataService implements AutoCloseable {
     }
 
     public BrokersInfo<BrokersInfo.BrokerId> getBrokerIdsFromZk() throws ZookeeperClientException {
-        final List<String> brokerIds = zkCli.getChildren(kafkaZkConnection.buildZkFullPath(KAFKA_BROKERS_IDS_ZK_RELATIVE_PATH));
+        final List<String> brokerIds = zkCli.getChildren(kafkaZkConnection.buildZkRootPath(KAFKA_BROKERS_IDS_ZK_RELATIVE_PATH));
         return BrokersInfo.brokerIds(brokerIds);
     }
 
     public Topics getTopicsFromZk() throws ZookeeperClientException {
-        final List<String> topics = zkCli.getChildren(kafkaZkConnection.buildZkFullPath(KAFKA_TOPICS_ZK_RELATIVE_PATH));
+        final List<String> topics = zkCli.getChildren(kafkaZkConnection.buildZkRootPath(KAFKA_TOPICS_ZK_RELATIVE_PATH));
         return topics == null ? new Topics(Collections.<String>emptyList()) : new Topics(topics);
     }
 
@@ -248,7 +248,7 @@ public class KafkaMetadataService implements AutoCloseable {
             return zkString;
         }
 
-        String buildZkFullPath(String zkRelativePath) {
+        String buildZkRootPath(String zkRelativePath) {
             if (zkRelativePath.startsWith("/")) {
                 return chRoot + zkRelativePath.substring(1);
             } else {
