b01239ad68543a3b6ffed948638e726051dbe8b3
Hugo Louro <hmclouro@gmail.com>
2016-10-22 15:19:05 -0700

    IOT-393: Capture Metadata from clusters       - HBaseMetadataServiceTest Almost Working

--- streams/catalog/src/test/java/com/hortonworks/iotas/streams/catalog/service/metadata/HBaseMetadataServiceTest.java
+++ streams/catalog/src/test/java/com/hortonworks/iotas/streams/catalog/service/metadata/HBaseMetadataServiceTest.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import mockit.Expectations;
 import mockit.Mocked;
@@ -28,7 +29,7 @@ import mockit.integration.junit4.JMockit;
 public class HBaseMetadataServiceTest {
     private static final Logger LOG = LoggerFactory.getLogger(HBaseMetadataServiceTest.class);
 
-    private final static String HBASE_TEST_NAMESPACE = "test_namespace";
+    private final static List<String> HBASE_TEST_NAMESPACES = ImmutableList.copyOf(new String[]{"test_namespace_1", "test_namespace_2"});
     private final static List<String> HBASE_TEST_TABLES = ImmutableList.copyOf(new String[]{"test_table_1", "test_table_2"});
     private final static String HBASE_TEST_TABLE_FAMILY = "test_table_family";
 
@@ -46,52 +47,74 @@ public class HBaseMetadataServiceTest {
 
         hbaseService = HBaseMetadataService.newInstance(catalogService, 1L);
 
-        hbaseService.createNamespace(HBASE_TEST_NAMESPACE);
-
-        for (String table : HBASE_TEST_TABLES) {
-            hbaseService.createTable(HBASE_TEST_NAMESPACE, table, HBASE_TEST_TABLE_FAMILY);
+        for (String namespace : HBASE_TEST_NAMESPACES) {
+            hbaseService.createNamespace(namespace);
+            for (String table : HBASE_TEST_TABLES) {
+                hbaseService.createTable(namespace, table, HBASE_TEST_TABLE_FAMILY);
+            }
         }
     }
 
     private void tearDown() throws Exception {
-        for (String table : HBASE_TEST_TABLES) {
-            hbaseService.disableTable(HBASE_TEST_NAMESPACE, table);
-            hbaseService.deleteTable(HBASE_TEST_NAMESPACE, table);
+        for (String namespace : HBASE_TEST_NAMESPACES) {
+            for (String table : HBASE_TEST_TABLES) {
+                hbaseService.disableTable(namespace, table);
+                hbaseService.deleteTable(namespace, table);
+            }
+            hbaseService.deleteNamespace(namespace);
         }
-
-        hbaseService.deleteNamespace(HBASE_TEST_NAMESPACE);
     }
 
     /*
         Calling all the tests in one method because table creation during setup is quite expensive and needs to be done
-        in the scope of the test because it depends on recorded expectations, which abstract lots of initialization.
+        in the scope of the test because it depends on recorded expectations in order to abstract lots of initialization.
      */
     @Test
     public void test_all() throws Exception {
         setUp();
         try {
-            test_getHBaseTablesForNamespace();
-            test_getHBaseNamespaces();
+            test_getHBaseTables();
+//            test_getHBaseTablesForNamespace();
+//            test_getHBaseNamespaces();
         } finally {
             tearDown();
         }
     }
+//        "A B C D"
+//        "1 2 3 4"
 
-    public void test_getHBaseTables() throws Exception {
+    private void test_getHBaseTables() throws Exception {
         Tables hBaseTables = hbaseService.getHBaseTables();
-        Assert.assertEquals(HBASE_TEST_TABLES.stream().map(p -> HBASE_TEST_NAMESPACE + ":" + p).collect(Collectors.toList()),
+        /*Assert.assertEquals(HBASE_TEST_NAMESPACES.stream().forEach(ns -> HBASE_TEST_TABLES.stream().map(stream -> ns + ":" + stream).collect(Collectors.toList())),
+                hBaseTables.getTables().stream().sorted(String::compareTo).collect(Collectors.toList()));*/
+
+        /*Assert.assertEquals(HBASE_TEST_NAMESPACES.stream().map(ns -> ns + ":" + HBASE_TEST_TABLES.stream().forEach(stream -> ns + ":" + stream).collect(Collectors.toList())),
                 hBaseTables.getTables().stream().sorted(String::compareTo).collect(Collectors.toList()));
+        */
+
+//        Stream.concat(HBASE_TEST_NAMESPACES.stream(), HBASE_TEST_TABLES.stream()).map((ns, st) -> ns + ":" + st.toString()).collect(Collectors.toList());
+
+        // HBASE_TEST_TABLES.stream().forEach(st -> System.out.println(ns + ":" + st))
+        Assert.assertEquals(HBASE_TEST_NAMESPACES.stream()
+//                .flatMap(ns -> Stream.of(ns.split("")))
+                .flatMap(ns -> HBASE_TEST_TABLES.stream().map(st -> ns + ":" + st))
+                .collect(Collectors.toList()),
+                hBaseTables.getTables().stream().sorted(String::compareTo).collect(Collectors.toList()));
+
+
+        /*Assert.assertEquals(HBASE_TEST_NAMESPACES.stream().map(HBASE_TEST_NAMESPACES.stream().peek())HBASE_TEST_NAMESPACES.stream().map(ns -> ns + ":" + HBASE_TEST_TABLES.stream().forEach(stream -> ns + ":" + stream).collect(Collectors.toList())),
+                hBaseTables.getTables().stream().sorted(String::compareTo).collect(Collectors.toList()));*/
     }
 
     private void test_getHBaseTablesForNamespace() throws Exception {
-        Tables hBaseTables = hbaseService.getHBaseTables(HBASE_TEST_NAMESPACE);
-        Assert.assertEquals(HBASE_TEST_TABLES.stream().map(p -> HBASE_TEST_NAMESPACE + ":" + p).collect(Collectors.toList()),
+        Tables hBaseTables = hbaseService.getHBaseTables(HBASE_TEST_NAMESPACES.get(0));
+        Assert.assertEquals(HBASE_TEST_TABLES.stream().map(p -> HBASE_TEST_NAMESPACES.get(0) + ":" + p).collect(Collectors.toList()),
                             hBaseTables.getTables().stream().sorted(String::compareTo).collect(Collectors.toList()));
     }
 
     private void test_getHBaseNamespaces() throws Exception {
         final HBaseMetadataService.Namespaces hBaseNamespaces = hbaseService.getHBaseNamespaces();
-        Assert.assertTrue(hBaseNamespaces.getNamespaces().contains(HBASE_TEST_NAMESPACE));
+        Assert.assertTrue(hBaseNamespaces.getNamespaces().containsAll(HBASE_TEST_NAMESPACES));
     }
 
     private Map<String, String> getHBaseConfiProps1() throws IOException {
