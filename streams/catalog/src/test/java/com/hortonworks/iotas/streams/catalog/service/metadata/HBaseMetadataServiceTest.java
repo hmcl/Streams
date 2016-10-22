1a0c9d48015a3aaefc04018b318dae0feb0be75b
Hugo Louro <hmclouro@gmail.com>
2016-10-22 11:36:31 -0700

    IOT-393: Capture Metadata from clusters       - HBaseMetadataServiceTest almost done.       - hivemetastore resource + ReadME.txt

--- streams/catalog/src/test/java/com/hortonworks/iotas/streams/catalog/service/metadata/HBaseMetadataServiceTest.java
+++ streams/catalog/src/test/java/com/hortonworks/iotas/streams/catalog/service/metadata/HBaseMetadataServiceTest.java
@@ -1,7 +1,6 @@
 package com.hortonworks.iotas.streams.catalog.service.metadata;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,20 +15,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import mockit.Expectations;
-import mockit.MockUp;
 import mockit.Mocked;
 import mockit.integration.junit4.JMockit;
 
 @RunWith(JMockit.class)
 public class HBaseMetadataServiceTest {
     private static final Logger LOG = LoggerFactory.getLogger(HBaseMetadataServiceTest.class);
+
+    private final static String HBASE_TEST_NAMESPACE = "test_namespace";
+    private final static List<String> HBASE_TEST_TABLES = ImmutableList.copyOf(new String[]{"test_table_1", "test_table_2"});
+    private final static String HBASE_TEST_TABLE_FAMILY = "test_table_family";
 
     private HBaseMetadataService hbaseService;
 
@@ -38,82 +39,9 @@ public class HBaseMetadataServiceTest {
     @Mocked
     private ServiceConfiguration serviceConfiguration;
 
-    /*@Before
-    public void setUp() throws Exception {
-        hBaseMetadataService = new HBaseMetadataService(
-                ConnectionFactory.createConnection(HBaseConfiguration.create()).getAdmin());
-    }*/
-
-    /*@Before
-    public void setUp() throws Exception {
-        hBaseMetadataService = new HBaseMetadataService(
-                ConnectionFactory.createConnection(HBaseConfiguration.create()).getAdmin());
-    }*/
-
-    class ServiceConfigurationMockUp extends MockUp<ServiceConfiguration> {
-
-        Map<String, String> getConfigurationMap() throws IOException {
-            return getHBaseConfiProps();
-        }
-    }
-
-
-    @Test
-    public void testMock2(/*@Mocked final StreamCatalogService catalogService, @Mocked ServiceConfiguration serviceConfiguration*/) throws Exception {
-
-        new Expectations() {{
-            serviceConfiguration.getConfigurationMap(); result = getHBaseConfiProps();
-        }};
-
-//        new ServiceConfigurationMockUp();
-
-        /*HBaseMetadataService*/ hbaseService = HBaseMetadataService.newInstance(catalogService, 1L);
-
-        final String namespace = "test_namespace";
-        hbaseService.createNamespace(namespace);
-
-        hbaseService.createTable(namespace, "test_table_1", "family");
-        hbaseService.createTable(namespace, "test_table_2", "family");
-        hbaseService.createTable(namespace, "test_table_3", "family");
-
-        hbaseService.deleteNamespace(namespace);
-
-        System.out.println("Namespaces: " + hbaseService.getHBaseNamespaces().getNamespaces());
-//        System.out.println("Namespaces: " + hBaseMetadataService.getHBaseNamespaces().getNamespaces());
-    }
-
-    private FileInputStream getHbaseSiteXmlFileInputStream1() throws FileNotFoundException {
-        final String filePath = "/Users/hlouro/Hortonworks/Tasks/IoTaS/ClusterMetadata/ConfigFiles/hbase-site.xml";
-        return new FileInputStream(filePath);
-    }
-
-    void log1(String msg, String... args) {
-        System.out.println(Thread.currentThread().getName() + " - " + Integer.toHexString(System.identityHashCode(this)) + " - " + String.format(msg, args));
-    }
-
-    final static String HBASE_TEST_NAMESPACE = "test_namespace";
-    final static List<String> HBASE_TEST_TABLES = ImmutableList.copyOf(new String[]{"test_table_1", "test_table_2"});
-    final static String HBASE_TEST_TABLE_FAMILY = "test_table_family";
-
-    // ===
-
-
-
-
-    @Test
-    public void getHBaseTables() throws Exception {
-        try {
-            setUp();
-            Tables hBaseTables = hbaseService.getHBaseTables();
-            System.out.println("Stop");
-        } finally {
-            tearDown();
-        }
-    }
-
     private void setUp() throws Exception {
         new Expectations() {{
-            serviceConfiguration.getConfigurationMap(); result = getHBaseConfiProps();
+            serviceConfiguration.getConfigurationMap(); result = getHBaseConfiProps1();
         }};
 
         hbaseService = HBaseMetadataService.newInstance(catalogService, 1L);
@@ -134,19 +62,9 @@ public class HBaseMetadataServiceTest {
         hbaseService.deleteNamespace(HBASE_TEST_NAMESPACE);
     }
 
-    @Test
-    public void callteardown() throws Exception {
-        new Expectations() {{
-            serviceConfiguration.getConfigurationMap(); result = getHBaseConfiProps();
-        }};
-
-        hbaseService = HBaseMetadataService.newInstance(catalogService, 1L);
-        tearDown();
-    }
-
     /*
-        Calling all the tests in one method because table creation during setup is quite expensive and needs to be done in the scope
-        of the test because it depends on recorded expectations, which abstract lots of initialization.
+        Calling all the tests in one method because table creation during setup is quite expensive and needs to be done
+        in the scope of the test because it depends on recorded expectations, which abstract lots of initialization.
      */
     @Test
     public void test_all() throws Exception {
@@ -157,32 +75,29 @@ public class HBaseMetadataServiceTest {
         } finally {
             tearDown();
         }
-
     }
 
-    @Test
     public void test_getHBaseTables() throws Exception {
-        Tables hBaseTables = hbaseService.getHBaseTables(HBASE_TEST_NAMESPACE);
+        Tables hBaseTables = hbaseService.getHBaseTables();
         Assert.assertEquals(HBASE_TEST_TABLES.stream().map(p -> HBASE_TEST_NAMESPACE + ":" + p).collect(Collectors.toList()),
                 hBaseTables.getTables().stream().sorted(String::compareTo).collect(Collectors.toList()));
     }
 
-    @Test
-    public void test_getHBaseTablesForNamespace() throws Exception {
+    private void test_getHBaseTablesForNamespace() throws Exception {
         Tables hBaseTables = hbaseService.getHBaseTables(HBASE_TEST_NAMESPACE);
         Assert.assertEquals(HBASE_TEST_TABLES.stream().map(p -> HBASE_TEST_NAMESPACE + ":" + p).collect(Collectors.toList()),
                             hBaseTables.getTables().stream().sorted(String::compareTo).collect(Collectors.toList()));
     }
 
-    @Test
-    public void test_getHBaseNamespaces() throws Exception {
+    private void test_getHBaseNamespaces() throws Exception {
         final HBaseMetadataService.Namespaces hBaseNamespaces = hbaseService.getHBaseNamespaces();
         Assert.assertTrue(hBaseNamespaces.getNamespaces().contains(HBASE_TEST_NAMESPACE));
     }
 
-    public static Map<String, String> getHBaseConfiProps() throws IOException {
-        final String path = "/Users/hlouro/Hortonworks/Dev/GitHub/hmcl/Streams/streams/catalog/src/test/java/com/hortonworks/iotas/streams/catalog/service/metadata/hbase-config-map.json";
-        Map<String, String> config = new ObjectMapper().readValue(new FileInputStream(path), new TypeReference<Map<String, String>>() { });
+    private Map<String, String> getHBaseConfiProps1() throws IOException {
+        Map<String, String> config = new ObjectMapper().readValue(Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("metadata/hivemetastore-site.json"),
+                new TypeReference<Map<String, String>>() { });
         return config;
     }
 }
---
+++ streams/catalog/src/test/resources/metadata/ReadME.txt
@@ -0,0 +1,6 @@
+hivemetastore-site.json
+    --> contains the JSON representation of the map containing the configuration for the hivemetastore
+        as returned by the method com.hortonworks.iotas.streams.catalog.ServiceConfiguration#getConfigurationMap
+    --> It contains the configuration for the cluster with
+            Ambari URL: "http://172.18.128.67:8080/api/v1/clusters/rmp_performance"
+            Credentials: admin:admin
